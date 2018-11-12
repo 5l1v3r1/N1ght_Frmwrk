2a6a9488f76f6db7dcd600ede0c3d76437fedb26
Putra _ <38994110+N1ght420@users.noreply.github.com>
2018-11-12 13:23:45 +0700

    Update N1.php

--- N1.php
+++ N1.php
@@ -1,16 +1,15 @@
 <?php
 
-include 'config.php';
-include 'main.php';
+include 'main/config.php';
+include 'main/main.php';
 
-index();
+indexold();
 
-echo "$cyan\n Menu$red >$white ";
-$input = trim(fgets(STDIN));
 /////////////////////////////////////////////////     Brute Menu     /////////////////////////////////////////////////
-if ($input == 1 OR $input == 01){
-    brute($input);
-    echo "$cyan\n Menu$red >$white ";
+function brutemenu(){
+    include 'main/config.php';
+    brute();
+    echo "$okegreen\n ($cyan Hax0r$okegreen )$white- on -$okegreen($white ~/Menu/Brute$okegreen ) \n$red >$white ";
     $input = trim(fgets(STDIN));
     if ($input == 1 OR $input == 01){
         logfin($input);
@@ -24,12 +23,17 @@ if ($input == 1 OR $input == 01){
     elseif ($input == 4 OR $input == 04){
         subdoscan($input);
     }
+    elseif ($input == 'cd ..' OR $input == 'cd' OR $input == 'home'){
+        index();
+        main();
+    }
 }
 /////////////////////////////////////////////////     Brute Menu     /////////////////////////////////////////////////
 /////////////////////////////////////////////////    Scanner Menu    /////////////////////////////////////////////////
-elseif ($input == 2 OR $input == 02){
-    scan($input);
-    echo "$cyan\n Menu$red >$white ";
+function scanmenu(){
+    include 'main/config.php';
+    scan();
+    echo "$okegreen\n ($cyan Hax0r$okegreen )$white- on -$okegreen($white ~/Menu/Scanner$okegreen ) \n$red >$white ";
     $input = trim(fgets(STDIN));
     if ($input == 1 OR $input == 01){
         whois($input);
@@ -43,40 +47,32 @@ elseif ($input == 2 OR $input == 02){
     elseif ($input == 4 OR $input == 04){
         nmap($input);
     }
+    elseif ($input == 'cd ..' OR $input == 'cd' OR $input == 'home'){
+        index();
+        main();
+    }
 }
 /////////////////////////////////////////////////    Scanner Menu    /////////////////////////////////////////////////
 /////////////////////////////////////////////////   Endecode  Menu   /////////////////////////////////////////////////
-elseif ($input == 3 OR $input == 03){
-    endecode($input);
-    echo "$cyan\n Menu$red >$white ";
+function endecodemenu(){
+    include 'main/config.php';
+    endecode();
+    echo "$okegreen\n ($cyan Hax0r$okegreen )$white- on -$okegreen($white ~/Menu/Crypto$okegreen ) \n$red >$white ";
     $input = trim(fgets(STDIN));
     if ($input == 1 OR $input == 01){
         encode($input);
     }
+    elseif ($input == 'cd ..' OR $input == 'cd' OR $input == 'home'){
+        index();
+        main();
+    }
 }
 /////////////////////////////////////////////////   Endecode  Menu   /////////////////////////////////////////////////
 /////////////////////////////////////////////////   Generator Menu   /////////////////////////////////////////////////
-elseif ($input == 4 OR $input == 04){
-    generator($input);
-    echo "$cyan\n Menu$red >$white ";
-    $input = trim(fgets(STDIN));
-    if ($input == 0 OR $input == 00){
-        custgen($input);
-    }
-}
-/////////////////////////////////////////////////   Generator Menu   /////////////////////////////////////////////////
-/////////////////////////////////////////////////    Miscellanous    /////////////////////////////////////////////////
-elseif ($input == 'h' OR $input == '0h'){
-    help($input);
-}
-elseif ($input == 'a' OR $input == '0a'){
-    about($input);
-}
-/////////////////////////////////////////////////    Miscellanous    /////////////////////////////////////////////////
-/////////////////////////////////////////////////       Bonus        /////////////////////////////////////////////////
-elseif ($input == 'Hax0r' OR $input == 'hax0r'){
-    mine($input);
-    echo "$okegreen\n ($cyan Hax0r$okegreen )$white- on -$okegreen($white ~/Menu/archive/mine$okegreen ) \n$red >$white ";
+function generatormenu(){
+    include 'main/config.php';
+    generator();
+    echo "$okegreen\n ($cyan Hax0r$okegreen )$white- on -$okegreen($white ~/Menu/Generator$okegreen ) \n$red >$white ";
     $input = trim(fgets(STDIN));
     if ($input == 1 OR $input == 01){
         bukalapak($input);
@@ -90,6 +86,47 @@ elseif ($input == 'Hax0r' OR $input == 'hax0r'){
     elseif ($input == 4 OR $input == 04){
         tokped($input);
     }
+    elseif ($input == 0 OR $input == 00){
+        custgen($input);
+    }
+    elseif ($input == 'cd ..' OR $input == 'cd' OR $input == 'home'){
+        index();
+        main();
+    }
 }
-/////////////////////////////////////////////////       Bonus        /////////////////////////////////////////////////
+/////////////////////////////////////////////////   Generator Menu   /////////////////////////////////////////////////
+function main(){
+include 'main/config.php';
+echo "$okegreen\n ($cyan Hax0r$okegreen )$white- on -$okegreen($white ~/Menu$okegreen ) \n$red >$white ";
+$input = trim(fgets(STDIN));
+/////////////////////////////////////////////////     Brute Menu     /////////////////////////////////////////////////
+if ($input == 1 OR $input == 01){
+    brutemenu();
+}
+/////////////////////////////////////////////////     Brute Menu     /////////////////////////////////////////////////
+/////////////////////////////////////////////////    Scanner Menu    /////////////////////////////////////////////////
+elseif ($input == 2 OR $input == 02){
+    scanmenu();
+}
+/////////////////////////////////////////////////    Scanner Menu    /////////////////////////////////////////////////
+/////////////////////////////////////////////////   Endecode  Menu   /////////////////////////////////////////////////
+elseif ($input == 3 OR $input == 03){
+    endecodemenu();
+}
+/////////////////////////////////////////////////   Endecode  Menu   /////////////////////////////////////////////////
+/////////////////////////////////////////////////   Generator Menu   /////////////////////////////////////////////////
+elseif ($input == 4 OR $input == 04){
+    generatormenu();
+}
+/////////////////////////////////////////////////   Generator Menu   /////////////////////////////////////////////////
+/////////////////////////////////////////////////    Miscellanous    /////////////////////////////////////////////////
+elseif ($input == 'h' OR $input == '0h'){
+    help($input);
+}
+elseif ($input == 'a' OR $input == '0a'){
+    about($input);
+}
+/////////////////////////////////////////////////    Miscellanous    /////////////////////////////////////////////////
+}
+main();
 ?>
