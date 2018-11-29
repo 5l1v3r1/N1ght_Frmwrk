d4ccef07609d0222445020219aa8c35c48c903e2
Putra _ <n1ghtpe0ple@protonmail.com>
2018-11-29 10:04:07 +0700

    Update encode.php

--- tools/crypto/encode.php
+++ tools/crypto/encode.php
@@ -3,34 +3,34 @@
 function encode(){
     include 'main/config.php';
     print "$okegreen\n█████████████████$white    Encode Tools$okegreen    █████████████████\n\n";
-    print "$cyan 01$red :$white Encode Md4\n";
-    print "$cyan 02$red :$white Encode Md5\n";
-    print "$cyan 03$red :$white Encode Sha1\n";
-    print "$cyan 04$red :$white Encode Base64\n";
-    print "$cyan 05$red :$white Encode All Type\n\n";
-    echo "$okegreen\n ($cyan Hax0r$okegreen )$white- on -$okegreen($white ~/Menu/Crypto/Encode$okegreen ) \n$red >$white ";
+    print "$cyan enmd4$red       :$white Encode Md4\n";
+    print "$cyan enmd5$red       :$white Encode Md5\n";
+    print "$cyan ensha1$red      :$white Encode Sha1\n";
+    print "$cyan enbase64$red    :$white Encode Base64\n";
+    print "$cyan enall$red       :$white Encode All Type\n\n";
+    echo "$red >$white ";
     $menu = trim(fgets(STDIN));
-    if ($menu == 1 OR $menu == 01){
+    if ($menu == 'enmd4'){
         echo "$cyan Input Kata$red >$white ";
         $base = trim(fgets(STDIN));
         echo "$cyan Encode Md4$red :$white ",crypt('md4',$base);
     }
-    elseif ($menu == 2 OR $menu == 02){
+    elseif ($menu == 'enmd5'){
         echo "$cyan Input Kata$red >$white ";
         $base = trim(fgets(STDIN,256));
         echo "$cyan Encode Md5$red :$white ",md5($base);
     }
-    elseif ($menu == 3 OR $menu == 03){
+    elseif ($menu == 'ensha1'){
         echo "$cyan Input Kata$red >$white ";
         $base = trim(fgets(STDIN));
         echo "$cyan Encode Sha1$red :$white ",sha1($base);
     }
-    elseif ($menu == 4 OR $menu == 04){
+    elseif ($menu == 'enbase64'){
         echo "$cyan Input Kata$red >$white ";
         $base = trim(fgets(STDIN));
         echo "$cyan Encode Md5$red :$white ",base64_encode($base);
     }
-    elseif ($menu == 5 OR $menu == 05){
+    elseif ($menu == 'enall'){
         echo "$cyan Input Kata$red >$white ";
         $base = trim(fgets(STDIN));
         print "$okegreen █$white Length$okegreen █████$white Type$okegreen █████████████$white Encode$okegreen █████████████$white\n";
@@ -39,13 +39,8 @@ function encode(){
             printf("    %3d         %-12s       %s\n\n", strlen($r), $v, $r);
         }
     }
-    elseif ($menu == 'cd ..'){
-        endecodemenu();
-    }
-    elseif ($menu == 'cd' OR $menu == 'home'){
-        index();
-        main();
-    }
+    print "\n\n";
+    main();
 }
 
 ?>
